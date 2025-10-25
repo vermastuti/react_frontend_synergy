7c165130f175354b3de595505f81506307f1e9f1
Ambika Garg <ambikagarg@Ambikas-MacBook-Pro.local>
2025-10-25 19:49:48 +0530

    UserRegisteration Changes Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import MovieDetails from "./pages/MovieDetails";
+import UserRegistration from "./components/UserRegistration";
 // import Booking from "./pages/Booking";
 // import Login from "./pages/Login";
 // import Register from "./pages/Register";
@@ -15,7 +16,7 @@ function App() {
           {/* <Route path="/movie/:id" element={<MovieDetails />} /> */}
           {/* <Route path="/book/:id" element={<Booking />} /> */}
           {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/register" element={<Register />} /> */}
+          <Route path="/register" element={<UserRegistration />} />
         </Routes>
       </MainLayout>
     </Router>
---
+++ src/components/UserRegistration.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react"
+import '../styles/userRegistration.css';
+import { useNavigate } from 'react-router-dom'
+
+export default function UserRegistration() {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState(
+        { firstName: "", lastName: "", mobileNo: "", email: "", password: "" }
+    )
+
+    const [error, setError] = useState({}); //state of errors
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUser({
+            ...user,
+            [name]: value
+        });
+    };
+
+
+    const validate = () => {
+        let errors = {};
+        if (!user.firstName.trim()) {
+            errors.firstName = "FirstName cannot be empty."
+        }
+
+        if (!user.lastName.trim()) {
+            errors.lastName = "LastName cannot be empty."
+        }
+
+        if (!user.mobileNo.trim()) {
+            errors.mobileNo = "Mobile Number cannot be empty."
+        }
+        else if (!/^[0-9]{10}$/.test(user.mobileNo)) {
+            errors.mobileNo = "Mobile number must be 10 digits";
+        }
+
+        if (!user.email.trim()) {
+            errors.email = "Email cannot be empty."
+        }
+        else if (!/\S+@\S+\.\S+/.test(user.email)) {
+            errors.email = "Please enter a valid email";
+        }
+
+        if (!user.password.trim()) {
+            errors.password = "Password cannot be empty."
+        }
+        else if (user.password.length < 8) {
+            errors.password = "Password must be at least 8 characters";
+        }
+
+        setError(errors); //setting error state
+        return Object.keys(errors).length === 0;
+
+    }
+
+    const handleRegister = () => {
+
+        if (validate()) {
+            sessionStorage.setItem("UserId", user.email);
+            navigate("/login");
+        }
+    }
+
+
+
+    return (
+        <div className="registration-container">
+            <div className="form-container">
+                <h1>Register</h1>
+
+                <label>
+                    FirstName:
+                    <input type="text" name="firstName" value={user.firstName} onChange={handleInputChange} ></input>
+                    {error.firstName ? <p className="error">{error.firstName}</p> : null}
+                </label>
+                <label> LastName:
+                    <input type="text" name="lastName" value={user.lastName} onChange={handleInputChange}></input>
+                    {error.lastName ? <p className="error">{error.lastName}</p> : null}
+                </label>
+                <label> MobileNo:
+                    <input type="tel" name="mobileNo" value={user.mobileNo} onChange={handleInputChange} ></input>
+                    {error.mobileNo ? <p className="error">{error.mobileNo}</p> : null}
+                </label>
+                <label> Email:
+                    <input type="email" name="email" value={user.email} onChange={handleInputChange}></input>
+                    {error.email ? <p className="error">{error.email}</p> : null}
+                </label>
+                <label> Password:
+                    <input type="password" name="password" value={user.password} onChange={handleInputChange}></input>
+                    {error.password ? <p className="error">{error.password}</p> : null}
+                </label>
+
+                <button onClick={handleRegister}>Register</button>
+
+            </div>
+        </div>
+    )
+}
